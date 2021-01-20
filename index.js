5efa50b910d78d290efdff9ccea063642ea7f441
drashti2210 <drashtijtandel@gmail.com>
2021-01-20 17:35:57 +0530

    Some changing in undone task

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 
 const app = express();
 
-const viewPath = path.join(__dirname,'./views');
+const viewPath = path.join(__dirname, './views');
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', viewPath)
@@ -17,6 +17,7 @@ app.post('/addtask', function (req, res) {
     let tasks = localStorage.getItem("todos");
     let duplicate;
     const newTask = req.body.newtask;
+
     if (tasks === null) {
         taskObj = [];
         duplicate = false;
@@ -38,26 +39,35 @@ app.post('/addtask', function (req, res) {
     res.redirect('/');
 });
 
+app.post('/readdtask', (req, res) => {
+    let tasks = localStorage.getItem("todos");
+    const undo = req.body.newTsk
+    if (tasks === null) {
+        taskObj = [];
+    } else {
+        taskObj = JSON.parse(tasks);
+    }
+    let key = taskObj.find((tsk) => tsk.task === undo);
+    key.isDone = false
+
+
+    localStorage.setItem("todos", JSON.stringify(taskObj));
+    res.redirect("/");
+})
+
 app.post("/donetask", function (req, res) {
     let tasks = localStorage.getItem("todos");
     const done = req.body.check1
-    
+
     if (tasks === null) {
         taskObj = [];
     } else {
         taskObj = JSON.parse(tasks);
     }
 
-    if (typeof done === "string") {
-        let key = taskObj.find((tsk) => tsk.task === done);
-        key.isDone = true
-    }
-    else if(typeof done === "object"){
-        for (let i = 0; i < done.length; i++) {
-            let ele = taskObj.find((tsk) => tsk.task === done[i]);
-            ele.isDone = true
-        }
-    }
+    let key = taskObj.find((tsk) => tsk.task === done);
+    key.isDone = true
+
     localStorage.setItem("todos", JSON.stringify(taskObj));
     res.redirect("/");
 });
@@ -72,16 +82,10 @@ app.post("/deletetask", function (req, res) {
         taskObj = JSON.parse(tasks);
     }
     console.log(toDelete)
-    if (typeof toDelete === "string") {
-        let k = taskObj.find((tsk) => tsk.task === toDelete);
-        taskObj.splice(taskObj.indexOf(k), 1)
-    }
-    else if(typeof toDelete === "object"){
-        for (let i = 0; i < toDelete.length; i++) {
-            let ele = taskObj.find((tsk) => tsk.task === toDelete[i]);
-            taskObj.splice(taskObj.indexOf(ele), 1)
-        }
-    }
+
+    let k = taskObj.find((tsk) => tsk.task === toDelete);
+    taskObj.splice(taskObj.indexOf(k), 1)
+
     localStorage.setItem("todos", JSON.stringify(taskObj));
     res.redirect("/");
 })
