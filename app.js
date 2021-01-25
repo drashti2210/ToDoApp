aa049b73820c2cf64e139f3dc1806b7f3f75e7ff
drashti2210 <drashtijtandel@gmail.com>
2021-01-25 10:47:17 +0530

    mongodbpractice

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
+mongoose.connect('mongodb://127.0.0.1:27017/ToDo-App', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+const taskRouter = require('./router')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
